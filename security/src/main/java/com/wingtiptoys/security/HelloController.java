0e996e7b3f2919328cb2b643153ed0aecabb355c
pawan1773 <joginder.pawan@gmail.com>
2019-12-22 19:02:53 +0530

    first

---
+++ security/src/main/java/com/wingtiptoys/security/HelloController.java
@@ -0,0 +1,30 @@
+package com.wingtiptoys.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@Autowired
+	@PreAuthorize("hasRole('Users')")
+	@RequestMapping("/")
+	public String helloWorld() {
+		return "Hello Users!";
+	}
+
+	@PreAuthorize("hasRole('Group1')")
+	@RequestMapping("/Group1")
+	public String groupOne() {
+		return "Hello Group 1 Users!";
+	}
+
+	@PreAuthorize("hasRole('Group2')")
+	@RequestMapping("/Group2")
+	public String groupTwo() {
+		return "Hello Group 2 Users!";
+	}
+
+}
